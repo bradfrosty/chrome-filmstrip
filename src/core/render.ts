0b473fb9ba1d2c1e0f944f8a7feadb52cb44294c
Brad Frost <code@bradfrost.me>
2022-08-15 10:45:48 -0400

    feat: burn timestamp into videos

--- src/core/render.ts
+++ src/core/render.ts
@@ -1,9 +1,14 @@
+import { fetchFile } from '@ffmpeg/ffmpeg';
 import { ffmpeg } from './ffmpeg.js';
 
 const IN_MEM_CONCAT_PATH = 'concat.txt';
 const IN_MEM_OUTPUT_PATH = 'output.mp4';
 
 export async function render(videos: Video[]): Promise<Uint8Array> {
+	// load font
+	ffmpeg.FS('writeFile', 'OpenSans.ttf', await fetchFile('./static/OpenSans.ttf'));
+
+	// render each profile as its own video using concat filter
 	const inMemoryVideoPaths: string[] = [];
 	for await (const [index, video] of videos.entries()) {
 		const concat = [];
@@ -22,25 +27,26 @@ export async function render(videos: Video[]): Promise<Uint8Array> {
 		const inMemoryVideoPath = `${index}-video.mp4`;
 		inMemoryVideoPaths.push(inMemoryVideoPath);
 		const videoCmd = [
+			// render snapshot images with concat filter
 			'-f',
 			'concat',
 			'-i',
 			IN_MEM_CONCAT_PATH,
+			// tell ffmpeg that chrome's timestamps are in microseconds
 			'-vf',
 			'settb=1/1000,setpts=PTS/1000',
-			'-vsync',
-			'vfr',
+			// set framerate to 60fps
 			'-r',
-			'1000',
+			'60',
 			inMemoryVideoPath,
 		];
 		await ffmpeg.run(...videoCmd);
 	}
 
-	const renderCmd = inMemoryVideoPaths.flatMap(videoPath => ['-i', videoPath]);
+	const inputs = inMemoryVideoPaths.flatMap(videoPath => ['-i', videoPath]);
 	const complexFilter = [];
 	const scale = Array.from(videos.keys()).map(index =>
-		`[${index}:v]scale=500:500:force_original_aspect_ratio=decrease,pad=640:720:-1:-1:color=black,setsar=sar=1${
+		`[${index}:v]scale=500:500:force_original_aspect_ratio=decrease,pad=640:720:-1:-1:color=black,setsar=sar=1,drawtext=text='%{pts\\:hms}': x=(w-tw)/2: y=h-(2*lh): fontsize=36: fontcolor=white: fontfile=OpenSans.ttf${
 			videos.length > 1 ? `[${index}:v:scaled]` : '[out]'
 		}`
 	).join(';');
@@ -50,6 +56,8 @@ export async function render(videos: Video[]): Promise<Uint8Array> {
 		const xstack = `${inputs}hstack=inputs=${videos.length}[out]`;
 		complexFilter.push(xstack);
 	}
+
+	const renderCmd = [...inputs];
 	renderCmd.push('-filter_complex', complexFilter.join(';'));
 	renderCmd.push('-map', '[out]');
 	renderCmd.push(IN_MEM_OUTPUT_PATH);
