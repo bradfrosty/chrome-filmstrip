c6b5a2dccc671151e6c2d8ae4b4bf64acf58e8bf
Brad Frost <code@bradfrost.me>
2022-08-16 10:22:54 -0400

    refactor: move postprocessing to first ffmpeg invocation

--- src/core/render.ts
+++ src/core/render.ts
@@ -4,11 +4,13 @@ import { ffmpeg } from './ffmpeg.js';
 const CONCAT_INPUT_PATH = 'concat.txt';
 const VIDEO_OUTPUT_PATH = 'output.mp4';
 const VIDEO_FONT_FILE = 'OpenSans.ttf';
+const VIDEO_FRAME_RATE = 60;
 const VIDEO_SCALE = '500:500';
-const VIDEO_PADDING = '640:720';
+const VIDEO_PADDING = '640:640';
 const VIDEO_BACKGROUND_COLOR = 'black';
 const VIDEO_FONT_COLOR = 'white';
-const VIDEO_FONT_SIZE = '24';
+const VIDEO_FONT_SIZE = 24;
+const DEFAULT_DRAWTEXT_ARGS = `fontsize=${VIDEO_FONT_SIZE}:fontcolor=${VIDEO_FONT_COLOR}:fontfile=${VIDEO_FONT_FILE}`;
 
 async function renderVideos(videos: Video[]): Promise<string[]> {
 	const videoPaths: string[] = [];
@@ -32,20 +34,35 @@ async function renderVideos(videos: Video[]): Promise<string[]> {
 		// write the concat filter input file to concat.txt
 		ffmpeg.FS('writeFile', CONCAT_INPUT_PATH, concatFilter.join('\n'));
 
+		const postprocessFilter = [
+			// set timebase on scale of microseconds
+			'settb=1/1000',
+			// do the same for presentation timestamps
+			'setpts=PTS/1000',
+			// convert variable fr to constant fr which will interpolate new output frames
+			// set the scene level to 0 — this prevents the interpolated frames from looking like an animation, which is inaccurate
+			`framerate=fps=${VIDEO_FRAME_RATE}:scene=0`,
+			// scale the video while, downscaling the original aspect ratio if needed
+			`scale=${VIDEO_SCALE}:force_original_aspect_ratio=decrease`,
+			// apply extra padding for text space, filling empty space with black background
+			`pad=${VIDEO_PADDING}:-1:-1:color=${VIDEO_BACKGROUND_COLOR}`,
+			// overlay video title (centered on the top)
+			`drawtext=text='${video.title}':x=(w-tw)/2:y=(1*lh):${DEFAULT_DRAWTEXT_ARGS}`,
+			// overlay frame timestamp (centered on the bottom)
+			`drawtext=text='%{pts\\:hms}':x=(w-tw)/2:y=h-(2.5*lh):${DEFAULT_DRAWTEXT_ARGS}`,
+		];
+
 		// execute ffmpeg with concat filter to render an individual video
 		const videoPath = `${index}.mp4`;
 		await ffmpeg.run(
-			// render snapshot images with concat filter
+			// render snapshot images into video with concat filter
 			'-f',
 			'concat',
 			'-i',
 			CONCAT_INPUT_PATH,
-			// tell ffmpeg that chrome's timestamps are in microseconds
+			// apply postprocess filtergraph
 			'-vf',
-			'settb=1/1000,setpts=PTS/1000',
-			// set framerate to 60fps
-			'-r',
-			'60',
+			postprocessFilter.join(','),
 			videoPath,
 		);
 		videoPaths.push(videoPath);
@@ -55,39 +72,12 @@ async function renderVideos(videos: Video[]): Promise<string[]> {
 
 async function renderCollage(videoPaths: string[]): Promise<Uint8Array> {
 	// an array representing the ffmpeg command for rendering the collage video
-	const cmd = [];
-	// define each video as an input
-	cmd.push(...videoPaths.flatMap(p => ['-i', p]));
-
-	const filterGraph = [];
-	let scaleChainOutputs = [];
-	for (const index in videoPaths) {
-		// TODO: drawtext twice for multiline
-		// TODO: perform this scale during the first ffmpeg invocation
-		let scaleAndTextFilterChain = `[${index}:v]scale=${VIDEO_SCALE}:force_original_aspect_ratio=decrease,`;
-		scaleAndTextFilterChain += `pad=${VIDEO_PADDING}:-1:-1:color=${VIDEO_BACKGROUND_COLOR},`;
-		scaleAndTextFilterChain += `setsar=sar=1,`; // do i need this?
-		scaleAndTextFilterChain +=
-			`drawtext=text='%{pts\\:hms}\nVideo ${index}': x=(w-tw)/2: y=h-(2*lh): fontsize=${VIDEO_FONT_SIZE}: fontcolor=${VIDEO_FONT_COLOR}: fontfile=${VIDEO_FONT_FILE}`;
-
-		// store chain output to use as input later
-		const output = videoPaths.length > 1 ? `[tmp:${index}:scaled]` : '[out]';
-		scaleChainOutputs.push(output);
-
-		scaleAndTextFilterChain += output;
-		filterGraph.push(scaleAndTextFilterChain);
-	}
-	// if more than one, create hstack chain to collage them. otherwise, just buffer them to [out].
-	// hstack doesn't work on a single video and we need to get the output of the chain synchronized
+	const cmd = [...videoPaths.flatMap(p => ['-i', p])];
+	// if more than one video, run into collage with hstack
 	if (videoPaths.length > 1) {
-		const collageChain = `${scaleChainOutputs.join('')} hstack=inputs=${videoPaths.length} [out]`;
-		filterGraph.push(collageChain);
+		cmd.push('-filter_complex', `hstack=inputs=${videoPaths.length}`);
 	}
-
-	cmd.push('-filter_complex', filterGraph.join(';'));
-	cmd.push('-map', '[out]');
 	cmd.push(VIDEO_OUTPUT_PATH);
-	console.log(cmd);
 	await ffmpeg.run(...cmd);
 	return ffmpeg.FS('readFile', VIDEO_OUTPUT_PATH);
 }
@@ -95,12 +85,8 @@ async function renderCollage(videoPaths: string[]): Promise<Uint8Array> {
 export async function render(videos: Video[]): Promise<Uint8Array> {
 	// load font
 	ffmpeg.FS('writeFile', VIDEO_FONT_FILE, await fetchFile(`./static/${VIDEO_FONT_FILE}`));
-
 	// render individual videos from frame data
 	const videoPaths = await renderVideos(videos);
-
-	// render individual videos into a collage
-	const collageVideo = await renderCollage(videoPaths);
-
-	return collageVideo;
+	// render individual videos into a collage (or just output to path if only one)
+	return renderCollage(videoPaths);
 }
